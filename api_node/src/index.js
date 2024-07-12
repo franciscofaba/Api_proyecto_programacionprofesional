3629305c6fef5571c8b044c710d6db0fc55aa177
franciscofaba <130718756+franciscofaba@users.noreply.github.com>
2024-07-11 21:42:25 -0400

    Update index.js

--- api_node/src/index.js
+++ api_node/src/index.js
@@ -1,33 +1,34 @@
-import express from 'express'
-import indexRoutes from './routes/index.routes.js'
-import UserRoutes from './routes/User.routes.js'
-import loginRoutes from './routes/login.routes.js'
-import CourseFromCareerRoutes from './routes/CourseFromCareer.routes.js'
-import careerRoutes from './routes/career.routes.js'
-import CourseInProgressRoutes from './routes/CourseInProgress.routes.js'
-import attendanceRoutes from './routes/attendance.routes.js'
-import regulationRoutes from './routes/regulation.routes.js'
-import informacioncursos from './routes/informacioncursos.routes.js'
-import pageroute from './routes/Page.routes.js'
-import Encuesta from './routes/Encuesta.routes.js'
-import chatRoutes from './routes/chatbot.routes.js' // Importa la nueva ruta
+import express from 'express';
+import indexRoutes from './routes/index.routes.js';
+import UserRoutes from './routes/User.routes.js';
+import loginRoutes from './routes/login.routes.js';
+import CourseFromCareerRoutes from './routes/CourseFromCareer.routes.js';
+import careerRoutes from './routes/career.routes.js';
+import CourseInProgressRoutes from './routes/CourseInProgress.routes.js';
+import attendanceRoutes from './routes/attendance.routes.js';
+import regulationRoutes from './routes/regulation.routes.js';
+import informacioncursos from './routes/informacioncursos.routes.js';
+import pageroute from './routes/Page.routes.js';
+import Encuesta from './routes/Encuesta.routes.js';
+import chatRoutes from './routes/chatbot.routes.js'; // Importa la nueva ruta
 import dotenv from "dotenv";
-import expressLayout from 'express-ejs-layouts'
+import expressLayout from 'express-ejs-layouts';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import pdfroutes from './routes/pdf.routes.js'
-import PreguntasyRespuestas from './routes/PreguntasyRespuestas.routes.js'
-import { validateToken } from './controller/settings/keys.js'
-import EnviarEmail from './routes/EnviarEmail.routes.js'
+import pdfroutes from './routes/pdf.routes.js';
+import PreguntasyRespuestas from './routes/PreguntasyRespuestas.routes.js';
+import { validateToken } from './controller/settings/keys.js';
+import EnviarEmail from './routes/EnviarEmail.routes.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 dotenv.config();
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -37,7 +38,7 @@ const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200'];
 app.use((req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin); 
+        res.setHeader('Access-Control-Allow-Origin', origin);
     }
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -46,20 +47,43 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api',  indexRoutes)
-app.use('/api', UserRoutes)
-app.use('/api', loginRoutes)
-app.use('/api', CourseFromCareerRoutes)
-app.use('/api', careerRoutes)
-app.use('/api', CourseInProgressRoutes)
-app.use('/api', attendanceRoutes)
-app.use('/api', regulationRoutes)
-app.use('/api', informacioncursos)
-app.use('/api', Encuesta)
-app.use('/api', chatRoutes) // Usa la nueva ruta para el chat
-app.use('/api', pageroute)
-app.use('/api', pdfroutes)
-app.use('/api', PreguntasyRespuestas)
-app.use('/api', EnviarEmail)
+// Servir archivos estáticos de la aplicación React (client)
+app.use('/client', express.static(join(__dirname, '../client/build')));
+
+// Servir archivos estáticos de la aplicación React (panel)
+app.use('/panel', express.static(join(__dirname, '../panel/build')));
+
+// API routes
+app.use('/api', indexRoutes);
+app.use('/api', UserRoutes);
+app.use('/api', loginRoutes);
+app.use('/api', CourseFromCareerRoutes);
+app.use('/api', careerRoutes);
+app.use('/api', CourseInProgressRoutes);
+app.use('/api', attendanceRoutes);
+app.use('/api', regulationRoutes);
+app.use('/api', informacioncursos);
+app.use('/api', Encuesta);
+app.use('/api', chatRoutes); // Usa la nueva ruta para el chat
+app.use('/api', pageroute);
+app.use('/api', pdfroutes);
+app.use('/api', PreguntasyRespuestas);
+app.use('/api', EnviarEmail);
+
+// Manejar todas las demás rutas y servir el index.html de React (client)
+app.get('/client/*', (req, res) => {
+    res.sendFile(join(__dirname, '../client/build', 'index.html'));
+});
+
+// Manejar todas las demás rutas y servir el index.html de React (panel)
+app.get('/panel/*', (req, res) => {
+    res.sendFile(join(__dirname, '../panel/build', 'index.html'));
+});
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
 export default app;
